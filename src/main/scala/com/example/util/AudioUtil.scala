b339d71154f8fa501eea2fb7274aecb5e79c4ffb
peiyan0 <peiyan0802@gmail.com>
2024-08-01 15:07:59 +0800

    refactor AudioUtil
    - improve efficiency
    - reduce functions used

--- src/main/scala/com/example/util/AudioUtil.scala
+++ src/main/scala/com/example/util/AudioUtil.scala
@@ -2,25 +2,17 @@ package com.example.util
 
 import com.example.MainApp
 import scalafx.scene.control.Button
-import scalafx.scene.image.ImageView
 
 object AudioUtil {
-  private var isMuted = false
-
   def handleMuteAction(muteBtn: Button, unmuteBtn: Button): Unit = {
-    isMuted = true
-    MainApp.mediaPlayer.setMute(isMuted)
-    updateMuteButtons(muteBtn, unmuteBtn)
+    MainApp.mediaPlayer.setMute(true)
+    muteBtn.setVisible(true)
+    unmuteBtn.setVisible(false)
   }
 
   def handleUnmuteAction(muteBtn: Button, unmuteBtn: Button): Unit = {
-    isMuted = false
-    MainApp.mediaPlayer.setMute(isMuted)
-    updateMuteButtons(muteBtn, unmuteBtn)
-  }
-
-  private def updateMuteButtons(muteBtn: Button, unmuteBtn: Button): Unit = {
-    unmuteBtn.visible = !isMuted
-    muteBtn.visible = isMuted
+    MainApp.mediaPlayer.setMute(false)
+    muteBtn.setVisible(false)
+    unmuteBtn.setVisible(true)
   }
 }
